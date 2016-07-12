0b99b4c62cde2ba5a624e9b55444f56798a805fd
doomcat55 <doomcat55@gmail.com>
2016-07-12 17:32:20 -0400

    debug

--- malcat/app.py
+++ malcat/app.py
@@ -3,7 +3,9 @@ from flask_cache import Cache
 import os
 
 app = Flask('malcat')
-app.config.update({'CACHE_TYPE': 'redis',
-                   'CACHE_REDIS_URL': os.getenv('REDIS_URL',
-                                                'redis://localhost:6379')})
+app.config.update({
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_URL': os.getenv('REDIS_URL', 'redis://localhost:6379'),
+    'DEBUG': True
+})
 cache = Cache(app)
