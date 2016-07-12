4054e7e1c61acd391ad9112331fdcf5563f54685
doomcat55 <doomcat55@gmail.com>
2016-07-12 19:23:33 -0400

    requests-cache

--- malcat/scrape.py
+++ malcat/scrape.py
@@ -5,12 +5,17 @@ import requests_cache
 from io import BytesIO
 from lxml import etree
 from collections import OrderedDict
+import redis
+import os
+
+redis_conn = redis.from_url(os.environ['REDIS_URL'])
 
 
 requests_cache.install_cache('malcat_cache',
                              backend='redis',
                              expire_after=900,
-                             old_data_on_error=True)
+                             old_data_on_error=True,
+                             connection=redis_conn)
 
 
 def list_nodes(username, list_type, target):
